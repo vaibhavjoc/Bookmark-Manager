d133f3810935e553ad8a248d3afb6ab9aa74d6f1
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-09-14 18:39:05 +0530

    store the auth jwt secret in env

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "thisIS78dbjbcdnkhis";
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
 
 function auth(req, res, next) {
     const token = req.headers.token;
