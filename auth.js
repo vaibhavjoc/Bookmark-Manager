18ed6d1f4863b37bde760e9be556ba52e3fe6ae6
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2025-09-14 16:53:50 +0530

    Add the Authorisation

--- auth.js
+++ auth.js
@@ -1,3 +1,28 @@
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "thisIS78dbjbcdnkhis";
 
+function auth(req, res, next) {
+    const token = req.headers.token;
+
+    if(!token) {
+        return res.json({
+            message: "No token Provided"
+        })
+    }
+
+    const decodedData = jwt.verify(token, JWT_SECRET);
+
+    if(decodedData) {
+        req.userId = decodedData.indexOf;
+        next()
+    } else {
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
+}
+
+module.exports = {
+    auth,
+    JWT_SECRET
+}
